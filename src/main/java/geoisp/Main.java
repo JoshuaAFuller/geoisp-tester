c1876909526ee3de35f4573cc26544fef69fd519
Joshua Fuller <Joshua.Fuller@netscout.com>
2021-04-16 10:37:51 -0600

    Init

---
+++ src/main/java/geoisp/Main.java
@@ -0,0 +1,25 @@
+package geoisp;
+
+import com.maxmind.geoip2.WebServiceClient;
+import com.maxmind.geoip2.model.CityResponse;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+
+public class Main {
+  private static int MAX_MIND_USER = 89293;
+  private static String MAX_MIND_LICENSE = "fZ0WHEC3fDoa";
+  
+  public static void main(String[] args) {
+      String strAddr = args[0];
+      WebServiceClient client = new WebServiceClient.Builder(
+          MAX_MIND_USER, MAX_MIND_LICENSE
+      ).build();
+      try {
+          InetAddress addr = Inet4Address.getByName(strAddr);
+          CityResponse cityResponse = client.city(addr);
+          System.out.println(cityResponse);
+      } catch(Throwable e) {
+          System.out.println(e);
+      }
+  }
+}
